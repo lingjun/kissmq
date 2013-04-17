0bdd0cb52f3619d9e61c8589692277e459dc185a
shupeng-li <shupeng.lisp@gmail.com>
2013-04-17 20:17:23 +0800

    amqp basic type

---
+++ kissmq-common/src/main/java/org/kissmq/common/type/basic/UnsignedByte.java
@@ -0,0 +1,18 @@
+/**
+ * %L% %Z%
+ *
+ * Copyright (c) 2013. All rights reserved.
+ * You may obtain more information at
+ *
+ *     http://www.kissmq.org/
+ *
+ */
+package org.kissmq.common.type.basic;
+
+/**
+ * @author Shupeng Lisp
+ *
+ */
+public class UnsignedByte {
+
+}
